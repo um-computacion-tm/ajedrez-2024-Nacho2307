b166eff8f94326909f98e034918f69a4df4359ab
Ignacio Aguilera <baigorriajayatignacio@gmail.com>
2024-08-11 20:42:53 -0300

    corrigiendo prueba coverage

---
+++ test.py
@@ -0,0 +1,15 @@
+import unittest
+from main import suma, resta
+
+class TestFunciones(unittest.TestCase):
+    def test_suma(self):
+        self.assertEqual(suma(5, 4),9)
+        self.assertEqual(suma(-1, 1),0)
+       
+    def test_suma(self):
+        self.assertEqual(resta(5, 4),1)
+        self.assertEqual(resta(-1, 1),0)
+        
+if __name__ == "__main__":
+    unittest.main()
+    
