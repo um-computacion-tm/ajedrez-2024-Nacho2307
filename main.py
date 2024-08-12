b163298cad847ece463c01c68eb9c440bd3bab8a
Ignacio Aguilera <baigorriajayatignacio@gmail.com>
2024-08-11 23:39:17 -0300

    prueba

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ def resta(a, b):
     return a - b
 if __name__ == "__main__":
     print(suma(5, 4))
+    print(resta(5, 4))
     
--- test.py
+++ test.py
@@ -1,24 +1,21 @@
 import unittest
 from main import suma, resta
+from util_pruebas import verificar_funcion
 
 class TestFunciones(unittest.TestCase):
-    def funcion_verificar(self, funcion, casos):
-        for a, b, resultado in casos:
-            with self.subTest(a=a, b=b, resultado=resultado):
-                self.assertEqual(funcion(a, b), resultado)
     def test_suma(self):
         casos_suma = [
             (5, 4, 9),
             (-1, 1, 0)
         ]
-        self.funcion_verificar(suma, casos_suma)
+        verificar_funcion(self, suma, casos_suma)
+    
     def test_resta(self):
         casos_resta = [
             (5, 4, 1),
             (-1, 1, -2)
         ]
-        self.funcion_verificar(resta, casos_resta)
+        verificar_funcion(self, resta, casos_resta)
 
 if __name__ == "__main__":
     unittest.main()
-    
