520314df79742e4c15aaae8abe8bf5e9ca366b8e
Ignacio Aguilera <baigorriajayatignacio@gmail.com>
2024-08-17 22:03:49 -0300

    Agregacion bishop y test

--- Juego/Piezas/Bishop.py
+++ Juego/Piezas/Bishop.py
@@ -1,4 +1,5 @@
-from Piezas.Piece import Piece
+from .Piece import Piece
+
 
 class Bishop(Piece):
     def __init__(self, color):
@@ -6,6 +7,8 @@ class Bishop(Piece):
     
     # El Alfil se mueve en diagonal
     def movimiento_correcto(self, from_row, from_col, to_row, to_col):
+        if from_row == to_row and from_col == to_col:
+            return False
         # Verifica si es en diagonal el movimiento
         return abs(to_row - from_row) == abs(to_col - from_col)
     
--- Test/test_bishop.py
+++ Test/test_bishop.py
@@ -5,20 +5,16 @@ class TestBishop(unittest.TestCase):
     def setUp(self):
         self.__bishop_white__ = Bishop("White")
     
-    def _test_move_diagonal(self, from_row, from_col, to_row, to_col, resultado_esperado):
-     self.assertEqual(self.__bishop_white__.movimiento_correcto(from_row, from_col, to_row, to_col), expected_result)
-    
     def test_move_diagonal_valido(self):
-     self._test_move_diagonal(1, 1, 4, 4, True)
-     self._test_move_diagonal(3, 5, 5, 3, True)
+        for from_row, from_col, to_row, to_col in [(1, 1, 4, 4), (3, 5, 5, 3)]:
+            self.assertTrue(self.__bishop_white__.movimiento_correcto(from_row, from_col, to_row, to_col))
     
     def test_move_diagonal_invalido(self):
-     self._test_move_diagonal(1, 1, 4, 3, False)
-     self._test_move_diagonal(3, 5, 4, 5, False)
+        for from_row, from_col, to_row, to_col in [(1, 1, 4, 3), (3, 5, 4, 5)]:
+            self.assertFalse(self.__bishop_white__.movimiento_correcto(from_row, from_col, to_row, to_col))
     
     def test_move_nulo(self):
         self.assertFalse(self.__bishop_white__.movimiento_correcto(4, 4, 4, 4))
 
 if __name__ == "__main__":
-    unittest.main()    
-        
+    unittest.main()
