4d3843b13a5dbb29cc238f9d0e2db70166450335
Ignacio Aguilera <baigorriajayatignacio@gmail.com>
2024-08-19 19:58:43 -0300

    Agregacion del movimiento del rey

--- Juego/Piezas/King.py
+++ Juego/Piezas/King.py
@@ -3,3 +3,11 @@ from Piezas.Piece import Piece
 class King(Piece):
     def __init__(self, color):
         super().__init__(color, "King", "K")
+        
+    def mover(self, x, y):
+        # Solo se puede moverse una casilla en cualquier direccion
+        if abs(x - self.x) <= 1 and abs(y - self.y) <= 1:
+            self.x = x
+            self.y = y
+        else:
+            print("Movimiento incorrecto")
--- Test/test_bishop.py
+++ Test/test_bishop.py
@@ -7,15 +7,16 @@ class TestBishop(unittest.TestCase):
     
     def _test_move_diagonal(self, expected_result, moves):
         for from_row, from_col, to_row, to_col in moves:
-            self.assertEqual(self.__bishop_white__.movimiento_correcto(from_row, from_col, to_row, to_col), expected_result)
+            with self.subTest(from_pos=(from_row, from_col), to_pos=(to_row, to_col)):
+                 self.assertEqual(self.__bishop_white__.movimiento_correcto(from_row, from_col, to_row, to_col), expected_result)
     
     def test_move_diagonal_valido(self):
-        valid_moves = [(1, 1, 4, 4), (3, 5, 5, 3)]
-        self._test_move_diagonal(True, valid_moves)
+        valido_move = [(1, 1, 4, 4), (3, 5, 5, 3)]
+        self._test_move_diagonal(True, valido_move)
     
     def test_move_diagonal_invalido(self):
-        invalid_moves = [(1, 1, 4, 3), (3, 5, 4, 5)]
-        self._test_move_diagonal(False, invalid_moves)
+        invalido_move = [(1, 1, 4, 3), (3, 5, 4, 5)]
+        self._test_move_diagonal(False, invalido_move)
 
 if __name__ == "__main__":
     unittest.main()
